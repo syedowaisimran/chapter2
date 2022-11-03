152a53c37b3f50ae00fe0a9950af3328adcf4588
syedowaisimran <oziimran34567@gmail.com>
2022-11-03 15:12:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var physic = +prompt("physic")
+var Math = +prompt("Math")
+var Chemistry = +prompt("Chemistry")
+var Islamait = +prompt("Islamait")
+var Urdu = +prompt("Urdu")
+console.log(physic)
+console.log(Math)
+console.log(Chemistry)
+console.log(Islamait)
+console.log(Urdu)
+
+var total = physic + Math + Chemistry + Islamait + Urdu
+console.log(500 * 100 / total);
+
+
+if (total == 500) {
+    console.log(+A)
+}
+
